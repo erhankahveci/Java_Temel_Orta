5aba5d18fda3bda4b1e1d5469bf4c72c2258a516
ErhanKahveci <116870663+erhankahveci@users.noreply.github.com>
2025-08-03 16:12:45 +0300

    Add files via upload

---
+++ javademos/datatypes/src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12;
+        sayi = 13;
+
+        byte sayi1 = 127;
+        sayi1 = -128;
+
+        float sayi2 =  13.2f;
+        sayi2 = 14.2f;
+        System.out.println(sayi2);
+
+        double sayi3 = 12.5;
+        sayi3 = 15.7;
+
+        char karakter = 'A';
+        karakter= 'B';
+        System.out.println(karakter);
+
+        String sehir = "Ankara";
+        sehir = "İstanbul";
+        System.out.println(sehir);
+
+        boolean dogruMu = false;
+        System.out.println(dogruMu);
+    }
+}
